be515429309c7e0a6d9d9ca8dd570cd70d77a199
mageunwon <geunwon1947@gmail.com>
2023-02-28 14:38:57 +0900

    리액트 중첩 라우트, outlet

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
 
 const App = () => {
   return (
@@ -9,6 +11,11 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="profiles/:username" element={<Profile />} />
+      <Route path="Articles" element={<Articles />} />
+      {/* <Route path="Articles/:id" element={<Article />} /> */}
+      <Route path="articles" element={<Articles />}>
+        <Route path=":id" element={<Article />} />
+      </Route>
     </Routes>
   );
 };
